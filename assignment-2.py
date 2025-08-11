452d3461717ef2ff79d1f2294af230d59f1d28ab
Travolta Sandei <67733769+sandei-travolta@users.noreply.github.com>
2025-08-11 14:03:46 +0300

    week-2 assignment

---
+++ assignment-2.py
@@ -0,0 +1,16 @@
+my_list = []  # Create an empty list
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+my_list.insert(1, 15)  # Insert 15 at the second position
+
+my_list.extend([50, 60, 70])  # Extend with another list
+
+my_list.pop()  # Remove the last element
+
+my_list.sort()  # Sort in ascending order
+
+index_30 = my_list.index(30)  # Find index of value 30
